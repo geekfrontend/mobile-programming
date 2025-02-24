1887711d49934e3646fb2b112bb12d759f530693
geekfrontend <geekfrontend16@gmail.com>
2025-02-24 11:19:40 +0800

    add materials

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,7 @@ const monoFont = Space_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "AriaDocs - Template",
-  metadataBase: new URL("https://ariadocs.vercel.app/"),
-  description:
-    "This comprehensive documentation template, crafted with Next.js and available as open-source, delivers a sleek and responsive design, tailored to meet all your project documentation requirements.",
+  title: "Programming Algorithm ",
 };
 
 export default function RootLayout({
